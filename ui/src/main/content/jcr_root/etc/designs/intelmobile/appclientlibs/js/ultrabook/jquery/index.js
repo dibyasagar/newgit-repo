2d5be8b92728e0c2593cdbea65843e4663c8d4cb
skarm1 <skarm1@d56a3af8-c092-443f-9987-f536df04eac3>
2012-10-11 10:00:17 +0000

    fix for S2,S3,Blackberry Menu and sticky menu scroll
    git-svn-id: https://del.sapient.resultspace.com/scm/intelmobile/branches/intelmobile_postprod@1939 d56a3af8-c092-443f-9987-f536df04eac3

--- ui/src/main/content/jcr_root/etc/designs/intelmobile/appclientlibs/js/ultrabook/jquery/index.js
+++ ui/src/main/content/jcr_root/etc/designs/intelmobile/appclientlibs/js/ultrabook/jquery/index.js
@@ -1,368 +1,426 @@
 /*
-	Author: James Ma
-	Author: Jeyhun Aliyev
-	Dependencies: jQuery, jQuery Mobile
-	Description: Intel Ultrabook Mobile
+    Author: James Ma
+    Author: Jeyhun Aliyev
+    Dependencies: jQuery, jQuery Mobile
+    Description: Intel Ultrabook Mobile
 
 */
 (
-	function( $names,$,undefined )
-	{
-		/* Menus */
+    function( $names,$,undefined )
+    {
+        /* Menus */
+
+        $names.Menus = function()
+        {
+            /* OOOOO */
+
+        var $class = this ;
+
+            /* OOOOO */
+
+        var $oMenus = {} ;
+        var $oContent = {} ;
+        var $oHeader = {} ;
+        var $oFooter = {} ;
+
+        var $oSticky = {} ;
+
+            /* OOOOO */
+
+        var $iCurScrollTop = 0 ;
+
+            /* OOOOO */
+
+        var xGetScrollTop = function()
+            {
+            var $iHtml = $("html").scrollTop() ;
+            var $iBody = $("body").scrollTop() ;
+            
+            
+         
+                    return ( $iHtml >= $iBody ) ? $iHtml : $iBody ;
+         
+            } ;
+
+            this.xSticky = function()
+            {
+            var $iScroll = xGetScrollTop() ;
+            
+            var isAndroidOS2=(navigator.userAgent.toLowerCase().indexOf("android") > -1) ? true : false;
+            var isBB= (navigator.userAgent.toLowerCase().indexOf("blackberry") > -1) ? true : false;
+            
+            setTimeout(function(){
+           
+            if(isAndroidOS2||isBB)
+            {
+           
+                if($("#menu-item").attr("class")=="active")
+                {
+                    
+                    var $iMenu=  $("#category-menu").height();
+                    $iScroll=$iMenu+30;
+                   
+                }
+                else if($("#search-item").attr("class")=="active")
+                {
+                    var $isearchMenu=  $("#search-menu").height();
+                    $iScroll=$isearchMenu+30;
+                   
+                }
+               
+           }
+           $iScroll = ( $iScroll <= 40 ) ? 40 : $iScroll ;
+           $oSticky.css( { "top":$iScroll +30 } ) ; 
+           },1000);
+                
+
+            } ;
+
+            this.xOpens = function()
+            {
+                $oMenus.show() ;
+
+                $iCurScrollTop = xGetScrollTop() ;
+                
+                
+                
+                $("html,body").scrollTop( 0 ) ;
+
+                $oContent.hide() ;
+                $oHeader.hide() ;
+                $oFooter.hide() ;
+               
+              
+
+            } ;
+
+            this.xClose = function( $b )
+            {
+                
+                var isiPhone3=(navigator.userAgent.match(/iPhone/i)) ? true : false;
+               
+                if(isiPhone3)
+                {
+                    var video=$('.video-container');
+                   
+                    if(video.length>0)
+                    {
+                        for(i=0;i<video.length;i++)
+                        {
+                          
+                           var newsrc=video[i].getElementsByTagName('iframe')[0].src;
+                           video[i].getElementsByTagName('iframe')[0].src=newsrc;
+                        }
+                    }
+                }
+                
+                $oHeader.show() ;
+                $oFooter.show() ;
+                $oContent.show() ;
+                
+              
 
-		$names.Menus = function()
-		{
-			/* OOOOO */
+                if( $b )
+                {
+                    $("html,body").scrollTop( $iCurScrollTop ) ;
 
-		var $class = this ;
+                }
 
-			/* OOOOO */
+                $oMenus.hide() ;
 
-		var $oMenus = {} ;
-		var $oContent = {} ;
-		var $oHeader = {} ;
-		var $oFooter = {} ;
+            } ;
 
-		var $oSticky = {} ;
+            /* OOOOO */
 
-			/* OOOOO */
+        var xBinds = function()
+            {
+                $oSticky.click
+                (
+                    function()
+                    {
+                        $class.xOpens() ;
 
-		var $iCurScrollTop = 0 ;
+                    }
 
-			/* OOOOO */
+                ) ;
 
-		var xGetScrollTop = function()
-			{
-			var $iHtml = $("html").scrollTop() ;
-			var $iBody = $("body").scrollTop() ;
+                $oMenus.find(".close").click
+                (
+                    function()
+                    {
+                        $class.xClose( true ) ;
 
-				return ( $iHtml >= $iBody ) ? $iHtml : $iBody ;
 
-			} ;
+                    }
 
-			this.xSticky = function()
-			{
-			var $iScroll = xGetScrollTop() ;
+                ) ;
 
-				$iScroll = ( $iScroll <= 40 ) ? 40 : 0 ;
-				$oSticky.css( { "top":$iScroll +30 } ) ;
+                $oMenus.find(".item").click
+                (
+                    function()
+                    {
+                        $class.xClose( false ) ;
 
-			} ;
+                    }
 
-			this.xOpens = function()
-			{
-				$oMenus.show() ;
+                ) ;
 
-				$iCurScrollTop = xGetScrollTop() ;
-				$("html,body").scrollTop( 0 ) ;
+                $(window).scroll
+                (
+                    function()
+                    {
+                        $class.xSticky() ;
 
-				$oContent.hide() ;
-				$oHeader.hide() ;
-				$oFooter.hide() ;
+                    }
 
-			} ;
+                ) ;
+                
+                 $(".dropdown-trigger").click
+                (
+                    function()
+                    {
+                        $class.xSticky() ;
 
-			this.xClose = function( $b )
-			{
-				$oHeader.show() ;
-				$oFooter.show() ;
-				$oContent.show() ;
+                    }
 
-				if( $b )
-				{
-					$("html,body").scrollTop( $iCurScrollTop ) ;
+                ) ;
 
-				}
+            } ;
 
-				$oMenus.hide() ;
+            /* OOOOO */
 
-			} ;
+        var xStart = function()
+            {
+                $oMenus = $(".menus") ;
+                $oContent = $(".content") ;
+                $oHeader = $("header") ;
+                $oFooter = $("footer") ;
 
-			/* OOOOO */
+                $oSticky = $(".sticky") ;
 
-		var xBinds = function()
-			{
-				$oSticky.click
-				(
-					function()
-					{
-						$class.xOpens() ;
+                xBinds() ;
 
-					}
+            } ;
 
-				) ;
+            xStart() ;
 
-				$oMenus.find(".close").click
-				(
-					function()
-					{
-						$class.xClose( true ) ;
 
 
-					}
+        } ;
 
-				) ;
+        /* Video */
 
-				$oMenus.find(".item").click
-				(
-					function()
-					{
-						$class.xClose( false ) ;
+        $names.Video = function()
+        {
+            /* OOOOO */
 
-					}
+        var $class = this ;
+        var $Bcove = window.brightcove ;
 
-				) ;
+            /* OOOOO */
 
-				$(window).scroll
-				(
-					function()
-					{
-						$class.xSticky() ;
+        var $oVideo = {} ;
+        var $oThumb = {} ;
 
-					}
+            /* OOOOO */
 
-				) ;
+        var xBinds = function()
+            {
+                $(window).bind
+                (
+                    "orientationchange",function()
+                    {
+                        $oVideo.hide() ;
+                        $oThumb.show() ;
 
-			} ;
+                    }
 
-			/* OOOOO */
+                ) ;
 
-		var xStart = function()
-			{
-				$oMenus = $(".menus") ;
-				$oContent = $(".content") ;
-				$oHeader = $("header") ;
-				$oFooter = $("footer") ;
+                $oThumb.click
+                (
+                    function()
+                    {
+                    var $iWidth = $( this ).width() ;
+                    var $iHeight = $( this ).height() ;
 
-				$oSticky = $(".sticky") ;
+                    var $oParent = $( this ).closest(".video-a") ;
+                    var $oPlays = $oParent.find(".video-aa") ;
+                    var $oDatas = $oPlays.attr("data-video") ;
 
-				xBinds() ;
+                    var $sVideo =
+                        '<object id="BCOVE'+$oDatas+'" class="BrightcoveExperience" >' +
 
-			} ;
+                            '<param name="bgcolor" value="#000000" />' +
+                            '<param name="playerID" value="1768181851001" />' +
+                            '<param name="playerKey" value="AQ~~,AAAAqwZd9wk~,X1Exj3sUi-19aVuw9KL_IGMiTkASW7rF" />' +
+                            '<param name="dynamicStreaming" value="true" />' +
+                            '<param name="videoSmoothing" value="true" />' +
+                            '<param name="wmode" value="transparent" />' +
+                            '<param name="isVid" value="true" />' +
+                            '<param name="isUI" value="true" />' +
 
-			xStart() ;
+                            '<param name="includeAPI" value="true" />' +
+                            '<param name="templateLoadHandler" value="vTempsLoads" />' +
+                            '<param name="htmlFallback" value="true" />' +
+                            '<param name="forceHTML5" value="true" />' +
 
+                            '<param name="@videoPlayer" value="'+$oDatas+'" />' +
 
+                            '<param name="height" value="'+$iHeight+'" />' +
+                            '<param name="width" value="'+$iWidth+'" />' +
 
-		} ;
+                        '</object>' ;
 
-		/* Video */
+                        $( this ).hide() ;
+                        $oPlays.empty().append( $sVideo ).show() ;
+                        $Bcove.createExperiences() ;
 
-		$names.Video = function()
-		{
-			/* OOOOO */
+                    }
 
-		var $class = this ;
-		var $Bcove = window.brightcove ;
+                ) ;
 
-			/* OOOOO */
+            } ;
 
-		var $oVideo = {} ;
-		var $oThumb = {} ;
+            /* OOOOO */
 
-			/* OOOOO */
+        var xStart = function()
+            {
+                $oVideo = $(".video-aa") ;
+                $oThumb = $(".video-ab") ;
 
-		var xBinds = function()
-			{
-				$(window).bind
-				(
-					"orientationchange",function()
-					{
-						$oVideo.hide() ;
-						$oThumb.show() ;
+                xBinds() ;
 
-					}
+            } ;
 
-				) ;
+            xStart() ;
 
-				$oThumb.click
-				(
-					function()
-					{
-					var $iWidth = $( this ).width() ;
-					var $iHeight = $( this ).height() ;
 
-					var $oParent = $( this ).closest(".video-a") ;
-					var $oPlays = $oParent.find(".video-aa") ;
-					var $oDatas = $oPlays.attr("data-video") ;
 
-					var $sVideo =
-						'<object id="BCOVE'+$oDatas+'" class="BrightcoveExperience" >' +
+        } ;
 
-							'<param name="bgcolor" value="#000000" />' +
-							'<param name="playerID" value="1768181851001" />' +
-							'<param name="playerKey" value="AQ~~,AAAAqwZd9wk~,X1Exj3sUi-19aVuw9KL_IGMiTkASW7rF" />' +
-							'<param name="dynamicStreaming" value="true" />' +
-							'<param name="videoSmoothing" value="true" />' +
-							'<param name="wmode" value="transparent" />' +
-							'<param name="isVid" value="true" />' +
-							'<param name="isUI" value="true" />' +
+        /* Mobil */
 
-							'<param name="includeAPI" value="true" />' +
-							'<param name="templateLoadHandler" value="vTempsLoads" />' +
-							'<param name="htmlFallback" value="true" />' +
-							'<param name="forceHTML5" value="true" />' +
+        $names.Mobil = function()
+        {
+            /* OOOOO */
 
-							'<param name="@videoPlayer" value="'+$oDatas+'" />' +
+        var $oLearn = {} ;
 
-							'<param name="height" value="'+$iHeight+'" />' +
-							'<param name="width" value="'+$iWidth+'" />' +
+            /* OOOOO */
 
-						'</object>' ;
+        var xBinds = function()
+            {
+                $oLearn.click
+                (
+                    function()
+                    {
+                    var $oParent = $( this ).closest(".slide") ;
 
-						$( this ).hide() ;
-						$oPlays.empty().append( $sVideo ).show() ;
-						$Bcove.createExperiences() ;
+                        if( !$( this ).hasClass("close") )
+                        {
+                            $( this ).addClass("close") ;
+                            $( this ).text( $( this ).attr("data-less") ) ;
+                            $oParent.find(".more").show() ;
 
-					}
+                        }
+                        else
+                        {
+                            $( this ).removeClass("close") ;
+                            $( this ).text( $( this ).attr("data-more") ) ;
+                            $oParent.find(".more").hide() ;
 
-				) ;
+                            $("html,body").scrollTop
+                            (
+                                ( $oParent.find(".inside").offset().top )-10
 
-			} ;
+                            ) ;
 
-			/* OOOOO */
+                        }
 
-		var xStart = function()
-			{
-				$oVideo = $(".video-aa") ;
-				$oThumb = $(".video-ab") ;
+                    }
 
-				xBinds() ;
+                ) ;
 
-			} ;
+            } ;
 
-			xStart() ;
+            /* OOOOO */
 
+        var xStart = function()
+            {
+                $(document).ready
+                (
+                    function()
+                    {
+                        $names.Menus =
+                        new $names.Menus() ;
 
+                        $names.Video =
+                        new $names.Video() ;
 
-		} ;
+                        $oLearn = $(".learn") ;
 
-		/* Mobil */
+                        xBinds() ;
 
-		$names.Mobil = function()
-		{
-			/* OOOOO */
+                    }
 
-		var $oLearn = {} ;
+                ) ;
 
-			/* OOOOO */
+                /* Address bar hide */
 
-		var xBinds = function()
-			{
-				$oLearn.click
-				(
-					function()
-					{
-					var $oParent = $( this ).closest(".slide") ;
+                $(window).load
+                (
+                    function()
+                    {
+                        //setTimeout( function(){ window.scrollTo(0,1) ; },0 ) ;
 
-						if( !$( this ).hasClass("close") )
-						{
-							$( this ).addClass("close") ;
-							$( this ).text( $( this ).attr("data-less") ) ;
-							$oParent.find(".more").show() ;
+                    }
 
-						}
-						else
-						{
-							$( this ).removeClass("close") ;
-							$( this ).text( $( this ).attr("data-more") ) ;
-							$oParent.find(".more").hide() ;
+                ) ;
 
-							$("html,body").scrollTop
-							(
-								( $oParent.find(".inside").offset().top )-10
+            } ;
 
-							) ;
+            xStart() ;
 
-						}
 
-					}
 
-				) ;
+        } ;
 
-			} ;
+        $names.Mobil =
+        new $names.Mobil() ;
 
-			/* OOOOO */
 
-		var xStart = function()
-			{
-				$(document).ready
-				(
-					function()
-					{
-						$names.Menus =
-						new $names.Menus() ;
 
-						$names.Video =
-						new $names.Video() ;
-
-						$oLearn = $(".learn") ;
-
-						xBinds() ;
-
-					}
-
-				) ;
-
-				/* Address bar hide */
-
-				$(window).load
-				(
-					function()
-					{
-						//setTimeout( function(){ window.scrollTo(0,1) ; },0 ) ;
-
-					}
-
-				) ;
-
-			} ;
-
-			xStart() ;
-
-
-
-		} ;
-
-		$names.Mobil =
-		new $names.Mobil() ;
-
-
-
-	}
+    }
 
 )
 (
-	window.$maliv=
-	window.$maliv||{},
-	jQuery
+    window.$maliv=
+    window.$maliv||{},
+    jQuery
 
 ) ;
 
 function vTempsLoads( $k )
 {
-	// console.log( $k ) ;
+    // console.log( $k ) ;
 
 // var $oModulA = brightcove.api.getExperience( $k ) ;
 
-	// $oModulM = $oModulA.getModule( brightcove.api.modules.APIModules.MEDIA ) ;
-	// $oModulV = $oModulA.getModule( brightcove.api.modules.APIModules.VIDEO_PLAYER ) ;
+    // $oModulM = $oModulA.getModule( brightcove.api.modules.APIModules.MEDIA ) ;
+    // $oModulV = $oModulA.getModule( brightcove.api.modules.APIModules.VIDEO_PLAYER ) ;
 
-	// $oModulM.addEventListener
-	// (
-		// brightcove.api.events.MediaEvent.PLAY ,
-		// function()
-		// {
-			// $oModulV.goFullScreen() ;
+    // $oModulM.addEventListener
+    // (
+        // brightcove.api.events.MediaEvent.PLAY ,
+        // function()
+        // {
+            // $oModulV.goFullScreen() ;
 
-		// }
+        // }
 
-	// ) ;
+    // ) ;
 
 
 
 }
-
-
-
-
