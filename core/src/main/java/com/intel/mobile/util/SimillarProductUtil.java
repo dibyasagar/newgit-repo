25bf34284ffa74692a3541416a85898851b456a6
sdubey <sdubey@d56a3af8-c092-443f-9987-f536df04eac3>
2012-12-20 10:14:37 +0000

    Corrected exceptions.
    git-svn-id: https://del.sapient.resultspace.com/scm/intelmobile/branches/intelmobile_postprod@2314 d56a3af8-c092-443f-9987-f536df04eac3

--- core/src/main/java/com/intel/mobile/util/SimillarProductUtil.java
+++ core/src/main/java/com/intel/mobile/util/SimillarProductUtil.java
@@ -24,112 +24,129 @@ import org.slf4j.LoggerFactory;
 import com.day.cq.wcm.api.Page;
 import com.intel.mobile.constants.IntelMobileConstants;
 import com.intel.mobile.vo.SmillarProductsVO;
-public class SimillarProductUtil {
-	
-	@Reference
-	
-	
-	private static final Logger LOG = LoggerFactory.getLogger(SimillarProductUtil.class);
-	
-	public static List getShopSimilarInfo(Page currentPage,ResourceResolver resolver) {
-		
-		String name = "";
-        String picture = "";
-        String bestPrice = "";
-	    String url = "";
-	    String id = "";
-	    String categoryPath = "";
-	    String prodName = "";
-	   
-	    Page categoryPage = currentPage.getParent();
-	    //LOG.info("-----categoryPage ----"+categoryPage.getPath());
-	    List<SmillarProductsVO> similarResultList = new ArrayList<SmillarProductsVO>();
-	   
-         try{
-        	 Node detailNode = currentPage.getContentResource().getChild("details").adaptTo(Node.class);
-        	 if(LOG.isDebugEnabled()) {
-     			LOG.debug("---detailNode-----"+detailNode.getName());
-        	 }
-             if(detailNode.hasNode("shopsimilarproducts")) {
-        		Node similarProductnode = detailNode.getNode("shopsimilarproducts");
-        		NodeIterator similarNode = similarProductnode.getNodes();
-        		//long Count = similarNode.getSize();
-        		
-        		while(similarNode.hasNext()){
-        			 Node tmpNode = similarNode.nextNode();
-        			if(tmpNode.hasProperty("name") && tmpNode.getProperty("name")!= null) {
-        			    	name = tmpNode.getProperty("name").getString();
-        			    	prodName = IntelUtil.normalizeName(name);
-        			    	//url = categoryPage.getPath()+"/"+prodName;
-        			    	url = getProductUrl(categoryPage,prodName,resolver);
-        			    }
-        			if(url != null && url != "")  { 
-        			    if(tmpNode.hasProperty("picture") && tmpNode.getProperty("picture") != null) {
-        			    	picture = tmpNode.getProperty("picture").getString();
-        			    }
-        			    if(tmpNode.hasProperty("bestPrice") && tmpNode.getProperty("bestPrice") != null) {
-        			    	bestPrice = tmpNode.getProperty("bestPrice").getString();
-        			    }
-        			   
-        			    SmillarProductsVO productsInfo = new SmillarProductsVO(name, picture,bestPrice,url);
-        			    if(!similarResultList.contains(productsInfo)) {
-        			    similarResultList.add(productsInfo);
-        			    }
-        			}
-        			//LOG.info("---similarResultList-----"+similarResultList);
-        			
-        			    if(LOG.isDebugEnabled()) {
-        					LOG.debug("---similarResultList-----"+similarResultList);
-        			    }
-        		   
-        		}
-        	}
-         
-        }
-         catch (RepositoryException e) {
- 			LOG.error("RepositoryException in getShopSimilarInfo :"+e.getMessage());
- 		}
-	
-         return similarResultList;
-}
-public  static String getProductUrl(Page categoryPage,String prodName,ResourceResolver resolver) {
-		
-        String productUrl = "";
-		//String queryStatement = "";
-		//String pageStats = "";
-		String tempUrl = categoryPage.getPath()+"/"+prodName;
-		Page reqPage = resolver.resolve(tempUrl).adaptTo(Page.class);
-	   if(reqPage.getProperties().get("cq:lastReplicationAction", "").equals("Activate")){
-		   productUrl = tempUrl;
-	   }
-		return productUrl;
-		
-	} 
 
-public static List getCmsSimilarProducts(String reqPages[],ResourceResolver resolver ) {
-	
-	 String pagePath = null;
-	 String name = "";
-     String picture = "";
-     String bestPrice = "";
-     String url = "";
-	 List<SmillarProductsVO> cmsSimilarList  = new ArrayList<SmillarProductsVO>();
-	 
-	 if (reqPages != null && reqPages.length >0)
-	 {
+public class SimillarProductUtil {
+
+	@Reference
+	private static final Logger LOG = LoggerFactory
+			.getLogger(SimillarProductUtil.class);
+
+	public static List getShopSimilarInfo(Page currentPage,
+			ResourceResolver resolver) {
+
+		String name = "";
+		String picture = "";
+		String bestPrice = "";
+		String url = "";
+		String id = "";
+		String categoryPath = "";
+		String prodName = "";
+
+		Page categoryPage = currentPage.getParent();
+		// LOG.info("-----categoryPage ----"+categoryPage.getPath());
+		List<SmillarProductsVO> similarResultList = new ArrayList<SmillarProductsVO>();
+
+		try {
+			Node detailNode = currentPage.getContentResource()
+					.getChild("details").adaptTo(Node.class);
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("---detailNode-----" + detailNode.getName());
+			}
+			if (detailNode.hasNode("shopsimilarproducts")) {
+				Node similarProductnode = detailNode
+						.getNode("shopsimilarproducts");
+				NodeIterator similarNode = similarProductnode.getNodes();
+				// long Count = similarNode.getSize();
+
+				while (similarNode.hasNext()) {
+					Node tmpNode = similarNode.nextNode();
+					if (tmpNode.hasProperty("name")
+							&& tmpNode.getProperty("name") != null) {
+						name = tmpNode.getProperty("name").getString();
+						prodName = IntelUtil.normalizeName(name);
+						// url = categoryPage.getPath()+"/"+prodName;
+						url = getProductUrl(categoryPage, prodName, resolver);
+					}
+					if (url != null && url != "") {
+						if (tmpNode.hasProperty("picture")
+								&& tmpNode.getProperty("picture") != null) {
+							picture = tmpNode.getProperty("picture")
+									.getString();
+						}
+						if (tmpNode.hasProperty("bestPrice")
+								&& tmpNode.getProperty("bestPrice") != null) {
+							bestPrice = tmpNode.getProperty("bestPrice")
+									.getString();
+						}
+
+						SmillarProductsVO productsInfo = new SmillarProductsVO(
+								name, picture, bestPrice, url);
+						if (!similarResultList.contains(productsInfo)) {
+							similarResultList.add(productsInfo);
+						}
+					}
+					// LOG.info("---similarResultList-----"+similarResultList);
+
+					if (LOG.isDebugEnabled()) {
+						LOG.debug("---similarResultList-----"
+								+ similarResultList);
+					}
+
+				}
+			}
+
+		} catch (Exception e) {
+			LOG.error("Exception in getShopSimilarInfo :" + e.getMessage());
+			LOG.debug(e.getMessage(), e);
+		}
+
+		return similarResultList;
+	}
+
+	public static String getProductUrl(Page categoryPage, String prodName,
+			ResourceResolver resolver) {
+		String productUrl = "";
+		try {
+			// String queryStatement = "";
+			// String pageStats = "";
+			String tempUrl = categoryPage.getPath() + "/" + prodName;
+			Page reqPage = resolver.resolve(tempUrl).adaptTo(Page.class);
+			if (reqPage.getProperties().get("cq:lastReplicationAction", "")
+					.equals("Activate")) {
+				productUrl = tempUrl;
+			}
+		} catch (Exception e) {
+			LOG.error("Exception getting product URL" + e.getMessage());
+			LOG.debug(e.getMessage(), e);
+		}
+		return productUrl;
+
+	}
+
+	public static List getCmsSimilarProducts(String reqPages[],
+			ResourceResolver resolver) {
+
+		String pagePath = null;
+		String name = "";
+		String picture = "";
+		String bestPrice = "";
+		String url = "";
+		List<SmillarProductsVO> cmsSimilarList = new ArrayList<SmillarProductsVO>();
+
+		if (reqPages != null && reqPages.length > 0) {
 			for (int i = 0; i < reqPages.length; i++) {
 				url = reqPages[i].toString();
-				if(LOG.isDebugEnabled()) {
+				if (LOG.isDebugEnabled()) {
 					LOG.debug("---url -----" + url.toString());
 				}
 				Resource root = resolver.getResource(url
 						+ "/jcr:content/details");
 				if (root != null) {
-					if(LOG.isDebugEnabled()) {
+					if (LOG.isDebugEnabled()) {
 						LOG.debug("---root -----" + root.getName());
 					}
 					ValueMap map = root.adaptTo(ValueMap.class);
-					if(LOG.isDebugEnabled()) {
+					if (LOG.isDebugEnabled()) {
 						LOG.debug("---Map-----" + map);
 					}
 					if (map.get("name") != null) {
@@ -141,18 +158,18 @@ public static List getCmsSimilarProducts(String reqPages[],ResourceResolver reso
 					if (map.get("bestPrice") != null) {
 						bestPrice = map.get("bestPrice").toString();
 					}
-				
-					SmillarProductsVO productsInfo = new SmillarProductsVO(name,
-							picture, bestPrice, url);
+
+					SmillarProductsVO productsInfo = new SmillarProductsVO(
+							name, picture, bestPrice, url);
 					cmsSimilarList.add(productsInfo);
 				}
-				if(LOG.isDebugEnabled()) {
+				if (LOG.isDebugEnabled()) {
 					LOG.debug("---cmsSimilarList-----" + cmsSimilarList);
 				}
 			}
 
 		}
-	  return cmsSimilarList; 	
-     }
+		return cmsSimilarList;
+	}
 
 }
