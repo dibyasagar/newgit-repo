eb68a3ff0a719dc99253ec04050396d77b4a9104
dibyasagar <dmoha1@BLRDMOHA177695.sapient.com>
2013-01-03 16:40:00 +0530

    removed core

--- core/src/main/java/com/intel/mobile/search/ISearchDAO.java
+++
@@ -1,5 +0,0 @@
-package com.intel.mobile.search;
-
-public interface ISearchDAO {
-    abstract SearchBeanList getResults (String url, String q1, String q2, String q3, String q4, String q10, String q11, String q23, String q24, String q26, String q40);
-}
