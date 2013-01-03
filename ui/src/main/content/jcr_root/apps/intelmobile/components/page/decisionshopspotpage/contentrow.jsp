95baf9f4804cfc6418c7815cb2d1ff51f2cd8b31
dibyasagar <dmoha1@BLRDMOHA177695.sapient.com>
2013-01-03 17:14:24 +0530

    deleted

--- ui/src/main/content/jcr_root/apps/intelmobile/components/page/decisionshopspotpage/contentrow.jsp
+++
@@ -1,8 +0,0 @@
-<%@include file="/libs/foundation/global.jsp"%>
-<%@page import="com.intel.mobile.util.IntelUtil"%>
-
-
-<!-- decisionshopspot inclusion Start -->
-<cq:include path="decisionshopspot"
-	resourceType="intelmobile/components/content/decisionshopspot" />
-<!-- decisionshopspot inclusion  End -->
--- ui/src/main/content/jcr_root/etc/designs/intelmobile/widget/custom-widget/source/Global.js
+++
@@ -1,5 +0,0 @@
-BLM={};
-BLM.Global={};
-BLM.Global.ListPagesSortOrderChanged=function(c,a){var b=this.findParentByType("dialog");
-alert("before submit?  path to  = "+c+", from dialog = "+this.findByType("dialog").path+", thispath11 = "+this.path+", form url = "+this.form.url+", record = "+a+", this = "+this.getField("./path"))
-};
